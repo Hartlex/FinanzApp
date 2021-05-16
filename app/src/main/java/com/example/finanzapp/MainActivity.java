5028176cc53d9d52839fbf39ba10c63b1799f06d
Alexander Hartmann <ahartmann@allegion.com>
2021-05-16 22:39:56 +0200

    Implemented BottomTabView and TopMenu

--- app/src/main/java/com/example/finanzapp/MainActivity.java
+++ app/src/main/java/com/example/finanzapp/MainActivity.java
@@ -2,11 +2,16 @@ package com.example.finanzapp;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.View;
 import android.view.Menu;
@@ -20,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        BottomNavigationView bottomNavView = findViewById(R.id.navigationView);
+        bottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                SelectFragment(menuItem);
+                return true;
+            }
+        });
 
     }
 
@@ -32,26 +45,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        DoSomething("test");
+        SelectFragment(item);
         return super.onOptionsItemSelected(item);
     }
-    /**
-     * Gibt eine Nachricht auf der Console aus.
-     *
-     *
-     * @author
-     * @param str Text der auf der Console ausgegeben wird.
-     */
-    public void DoSomething(String str){
-        //TODO implement this method
+    private void SelectFragment(MenuItem item){
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_main:
+                ChangeFragment(MainFragment.newInstance("",""));
+                return;
+            case R.id.action_transactions:
+                ChangeFragment(TransactionFragment.newInstance("",""));
+                return;
+            case R.id.action_settings:
+                ChangeFragment(SettingsFragment.newInstance("",""));
+                return;
+        }
+    }
+    private void ChangeFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer,fragment, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("name") // name can be null
+                .commit();
     }
 }
