f3e131c1e60927683f0c21cee55f81aa5eabf4a0
Alexander Hartmann <ahartmann@allegion.com>
2021-05-21 12:21:48 +0200

    Scrum Commit

--- app/src/main/java/com/example/finanzapp/database/Database.java
+++ app/src/main/java/com/example/finanzapp/database/Database.java
@@ -1,5 +1,13 @@
 package com.example.finanzapp.database;
 
-public class Database {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
+public class Database {
+    private SQLiteDatabase _database;
+    private DatabaseHelper _helper;
+
+    public Database(Context context){
+        _helper = new DatabaseHelper(context);
+    }
 }
--- app/src/main/java/com/example/finanzapp/database/DatabaseHelper.java
+++ app/src/main/java/com/example/finanzapp/database/DatabaseHelper.java
@@ -20,4 +20,6 @@ public class DatabaseHelper extends SQLiteOpenHelper
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+
 }
