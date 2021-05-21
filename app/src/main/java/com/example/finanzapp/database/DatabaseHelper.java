f323496564c19a93d3a4c44a1546dd7e16a4255e
Alexander Hartmann <ahartmann@allegion.com>
2021-05-21 11:45:42 +0200

    Added Constructor to DatabaseHelper.java

--- app/src/main/java/com/example/finanzapp/database/DatabaseHelper.java
+++ app/src/main/java/com/example/finanzapp/database/DatabaseHelper.java
@@ -1,10 +1,14 @@
 package com.example.finanzapp.database;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper
 {
+    public DatabaseHelper(Context context){
+        super(context,"PlaceholderDB",null,1);
+    }
     @Override
     public void onCreate(SQLiteDatabase db) {
 
