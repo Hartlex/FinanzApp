62e0c04703f2b42833241472c3b204e311ed741c
FR_DI <diseri.francesco@dh-karlsruhe.de>
2021-05-24 13:02:42 +0200

    Toolbar, Navigation bar entsprechend nach dem icon gestaltet. Refactorings an der activity_add_entry.xml vorgenommen

--- app/src/main/java/com/example/finanzapp/AddEntryActivity.java
+++ app/src/main/java/com/example/finanzapp/AddEntryActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
@@ -31,7 +32,7 @@ public class AddEntryActivity extends AppCompatActivity {
         Intent intent = getIntent();
         isExpense = intent.getBooleanExtra("isExpense",true);
 
-
+      
         //Elemente initialisieren
         InitSpinner();
         InitDatePicker();
