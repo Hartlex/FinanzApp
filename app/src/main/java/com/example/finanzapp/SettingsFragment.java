f9a4407b7f8ceccb54cbe1ddad3ba4e5f66db1fd
FR_DI <diseri.francesco@dh-karlsruhe.de>
2021-06-04 15:38:02 +0200

    In fragment_settings.xml  Elemente hinzugefügt und in SettingsFragment vorab schon den Pseudocode für das entfernen und hinzufügen von Kategorien geschrieben

--- app/src/main/java/com/example/finanzapp/SettingsFragment.java
+++ app/src/main/java/com/example/finanzapp/SettingsFragment.java
@@ -7,6 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,10 +23,17 @@ import android.view.ViewGroup;
  */
 public class SettingsFragment extends Fragment {
 
+    Button AddCategory, RemoveCategory;
+    EditText AddRemove;
+    Spinner sp;
+    // TODO: Retrieve the objects from the other spinner element
+    ArrayList list=new ArrayList(Arrays.asList(  ));
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -49,11 +64,44 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        /**
+        ArrayAdapter adapter  = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,list);
+
+         * add and remove each object for the category
+         *
+         * @author Di Seri.F
+
+
+        AddCategory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = AddRemove.getText().toString();
+                list.add(s);
+                adapter.notifyDataSetChanged();
+                sp.setAdapter(adapter);
+                Toast.makeText(getApplicationContext(), "Kategorie wurde hinzugefügt", Toast.LENGTH_LONG.show());
+            }
+        });
+
+            RemoveCategory.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String s = AddRemove.getText().toString();
+                    list.remove(s);
+                    adapter.notifyDataSetChanged();
+                    sp.setAdapter(adapter);
+                    Toast.makeText(getApplicationContext(), "Kategorie wurde entfernt", Toast.LENGTH_LONG.show());
+                }
+            });
+         * */
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
